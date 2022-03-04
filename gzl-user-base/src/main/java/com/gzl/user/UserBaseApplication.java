0ce2cbebcdfa3338513d8d59ad7ac982c97ec420
gaozhanlu <18330808841@163.com>
2022-03-04 23:10:30 +0800

    nacos整合

--- gzl-user-base/src/main/java/com/gzl/user/UserBaseApplication.java
+++ gzl-user-base/src/main/java/com/gzl/user/UserBaseApplication.java
@@ -2,9 +2,11 @@ package com.gzl.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserBaseApplication {
     public static void main(String[] ages) {
         SpringApplication.run(UserBaseApplication.class);
