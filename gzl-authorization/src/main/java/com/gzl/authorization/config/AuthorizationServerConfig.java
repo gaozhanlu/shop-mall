38ab078852fd010c650cb6293d7006efcb402c45
gaozhanlu <18330808841@163.com>
2022-03-25 11:28:25 +0800

    配置

--- gzl-authorization/src/main/java/com/gzl/authorization/config/AuthorizationServerConfig.java
+++ gzl-authorization/src/main/java/com/gzl/authorization/config/AuthorizationServerConfig.java
@@ -1,14 +1,43 @@
 package com.gzl.authorization.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 
 @Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-public class AuthorizationServerConfig  {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
+        clients
+                //客户端信息存放在内存
+                .inMemory()
+                //客户端账号(非用户账号)
+                .withClient("clientId")
+                //客户端密码（非用户密码）
+                .secret(passwordEncoder.encode("123456"))
+                //授权模式为授权码模式
+                .authorizedGrantTypes("authorization_code", "password", "refresh_token")
+                //接受或者拒绝后重定向地址
+                .redirectUris("http://localhost:8081/login")
+                //表示要求的授权范围
+                .scopes("all")
+                //设置token令牌过期时间
+                .accessTokenValiditySeconds(60)
+                //设置刷新token令牌过期时间
+                .refreshTokenValiditySeconds(48000)
+                .autoApprove(true);
 
+    }
 
 
 }
---
+++ gzl-authorization/src/main/java/com/gzl/authorization/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.gzl.authorization.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .permitAll();
+
+        http.authorizeRequests()
+                .antMatchers("/oauth/**", "/login/**","/logout/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf()
+                .disable();
+
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordWncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager getAuthenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}
