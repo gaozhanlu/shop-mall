920c6d86cccea5732fb7b66b91ce26d63446074b
gaozhanlu <18330808841@163.com>
2022-05-20 14:50:31 +0800

    配置

--- gzl-authorization-uaa/src/main/java/com/gzl/uaa/config/AuthorizationServer.java
+++ gzl-authorization-uaa/src/main/java/com/gzl/uaa/config/AuthorizationServer.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -63,17 +64,26 @@ public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
     @Override
     public void configure(ClientDetailsServiceConfigurer clients)
             throws Exception {
-        clients.withClientDetails(clientDetailsService);
+//        clients.withClientDetails(clientDetailsService);
+
+        clients.inMemory().withClient("c1").secret("secret")
+                .authorizedGrantTypes("password")
+                .scopes("all");
+
+
        /* clients.inMemory()// 使用in-memory存储
                 .withClient("c1")// client_id
                 .secret(new BCryptPasswordEncoder().encode("secret"))//客户端密钥
                 .resourceIds("res1")//资源列表
-                .authorizedGrantTypes("authorization_code", "password","client_credentials","implicit","refresh_token")// 该client允许的授权类型authorization_code,password,refresh_token,implicit,client_credentials
+                //.authorizedGrantTypes("authorization_code", "password","client_credentials","implicit","refresh_token")// 该client允许的授权类型authorization_code,password,refresh_token,implicit,client_credentials
+                .authorizedGrantTypes("password")
                 .scopes("all")// 允许的授权范围
                 .autoApprove(false)//false跳转到授权页面
                 //加上验证回调地址
-                .redirectUris("http://www.baidu.com")*/
-                ;
+                .redirectUris("http://www.baidu.com")
+                ;*/
+
+
     }
 
 
