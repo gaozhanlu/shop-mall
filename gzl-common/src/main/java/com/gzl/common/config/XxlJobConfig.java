ec6abac2e43147432d29ce554dd46dfc45bb1ec3
gaozhanlu <gaozhanlu@inno-chem.com.cn>
2022-07-01 20:24:56 +0800

    定时任务

--- gzl-common/src/main/java/com/gzl/common/config/XxlJobConfig.java
+++ gzl-common/src/main/java/com/gzl/common/config/XxlJobConfig.java
@@ -14,19 +14,19 @@ public class XxlJobConfig {
     @Value("${xxl.job.admin.addresses}")
     private String adminAddresses;
 
-    @Value("${xxl.job.accessToken}")
+    @Value("${xxl.job.accessToken:}")
     private String accessToken;
 
     @Value("${xxl.job.executor.appname}")
     private String appname;
 
-    @Value("${xxl.job.executor.address}")
+    @Value("${xxl.job.executor.address:}")
     private String address;
 
-    @Value("${xxl.job.executor.ip}")
+    @Value("${xxl.job.executor.ip:}")
     private String ip;
 
-    @Value("${xxl.job.executor.port}")
+    @Value("${xxl.job.executor.port:}")
     private int port;
 
     @Value("${xxl.job.executor.logpath}")
